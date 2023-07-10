d868e3ad09eec1b9f4936b90e2a4e595ae56d5b3
ayuriK152 <77537017+ayuriK152@users.noreply.github.com>
2023-07-11 04:18:25 +0900

    feat: 대화 시스템 상호작용 테스트를 위한 UI 및 스크립트 추가

--- WhoAreYou/Assets/Scripts/Datas.cs
+++ WhoAreYou/Assets/Scripts/Datas.cs
@@ -1,9 +1,11 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Datas
 {
+    [Serializable]
     public class DialogueData
     {
         public List<string> data;
