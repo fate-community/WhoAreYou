a4ba8bac92642cb073743438fe0fcca340d06b4e
ayuriK152 <77537017+ayuriK152@users.noreply.github.com>
2023-07-11 03:37:19 +0900

    feat: 대화 시스템을 위한 데이터 클래스 선언

---
+++ WhoAreYou/Assets/Scripts/Datas.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Datas
+{
+    public class DialogueData
+    {
+        public List<string> data;
+
+        public DialogueData()
+        {
+            data = new List<string>();
+        }
+    }
+}
