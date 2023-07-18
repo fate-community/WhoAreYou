d158bb2f8afa9da5430a26ec9cf7713892cb6f1f
golapaduck <golapaduck29@naver.com>
2023-07-18 21:40:55 +0900

    feat: CameraController 추가

---
+++ WhoAreYou/Assets/Scripts/Controller/CameraController.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public GameObject player;
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+
+    }
+}
