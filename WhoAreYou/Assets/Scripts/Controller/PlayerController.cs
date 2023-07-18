e13e71f65c666b4a6fb6e54e471b9ec3b3394ca5
golapaduck <golapaduck29@naver.com>
2023-07-18 21:01:15 +0900

    feat: 플레이어 이동 구현

--- WhoAreYou/Assets/Scripts/Controller/PlayerController.cs
+++ WhoAreYou/Assets/Scripts/Controller/PlayerController.cs
@@ -5,9 +5,13 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
 
+    [SerializeField]
+    float _speed = 10.0f;
+
     void Start()
     {
-
+        Managers.Input.KeyAction -= OnKeyboard;
+        Managers.Input.KeyAction += OnKeyboard;
     }
 
     void Update()
@@ -15,4 +19,27 @@ public class PlayerController : MonoBehaviour
 
     }
 
+    void OnKeyboard(KeyCode key)
+    {
+        if (key == KeyCode.W)
+        {
+            transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(Vector3.forward), 0.2f);
+            transform.position += Vector3.forward * Time.deltaTime * _speed;
+        }
+        if (key == KeyCode.S)
+        {
+            transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(Vector3.back), 0.2f);
+            transform.position += Vector3.back * Time.deltaTime * _speed;
+        }
+        if (key == KeyCode.A)
+        {
+            transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(Vector3.left), 0.2f);
+            transform.position += Vector3.left * Time.deltaTime * _speed;
+        }
+        if (key == KeyCode.D)
+        {
+            transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(Vector3.right), 0.2f);
+            transform.position += Vector3.right * Time.deltaTime * _speed;
+        }
+    }
 }
